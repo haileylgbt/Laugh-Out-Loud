2351653852a8951d634a8d7687d8a7b5fdeb6678
harrycode <pacboy2004@gmail.com>
2018-03-29 17:25:36 +1000

    more things! yay!

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var money = 0;
 var friendMoney = 0;
 var friendLimit = 10;
 var friendDoesChore = false;
+var librarian = {balance = 500}
 
 function chore(){
     money += 1;
@@ -15,15 +16,20 @@ function askFriend() {
   } else if (friendMoney === friendLimit) {
     window.alert("I already got the money! Go collect it!")
   }
+}
 
 function collectMoney() {
+  if (friendDoesChore === false) {
     money += friendMoney;
     friendMoney = 0;
     document.getElementById("money").innerHTML = money;
     document.getElementById("friendMoney").innerHTML = friendMoney;
+  }
 }
 
-};
+function sellBook() {
+ // TODO: Able to sell books :D
+}
 
 function friendChore() {
  if (friendDoesChore) {
@@ -41,11 +47,14 @@ var books = 0;
 function printBook(){
     var bookCost = Math.floor(10 * Math.pow(1.1,books));     //works out the cost of this book
     if(money >= bookCost){                                   //checks that the player can afford the book
-        books = books + 1;                                   //increases number of books
+        books = books + 1;
+        window.alert("'Ere you go! Fresh off the press! But it's gonna cost ya more next time!")                                 //increases number of books
     	money = money - bookCost;                          //removes the money spent
         document.getElementById('books').innerHTML = books;  //updates the number of books for the user
         document.getElementById('money').innerHTML = money;  //updates the number of money for the user
-    };
+    } else {
+      window.alert("Hmmmm, sorry bud. Don' have the funds.")
+    }
     var nextCost = Math.floor(10 * Math.pow(1.1,books));       //works out the cost of the next book
     document.getElementById('bookCost').innerHTML = nextCost;  //updates the book cost for the user
 };
